eab129538036361381704e6ea2c6af2fd442cc01
MinusKelvin <mark.carlson@minuskelvin.net>
2026-04-15 17:45:16 +1000

    shuffle packings for more representative set for time estimation

--- src/sixline2/placements.rs
+++ src/sixline2/placements.rs
@@ -5,6 +5,7 @@ use std::time::Instant;
 
 use foldhash::HashMap;
 use pcf::PieceSet;
+use rand::prelude::*;
 
 use crate::archive::{Archive, Dominance};
 use crate::parse_seq;
@@ -17,31 +18,34 @@ pub fn compute_placements(packings_file: PathBuf) {
     let set: PieceSet = parse_seq(set).unwrap().into_iter().collect();
     let set = format!("{set}");
 
-    let packings: Vec<[RawPlacement; 15]> =
+    let mut packings: Vec<[RawPlacement; 15]> =
         bytemuck::cast_vec(std::fs::read(packings_file).unwrap());
+    packings.shuffle(&mut rand::rngs::Xoshiro256PlusPlus::from_seed([
+        0xef, 0xdb, 0xe1, 0xab, 0x15, 0xb8, 0x73, 0x60, 0xf1, 0xc7, 0x65, 0x81, 0xf6, 0xb4, 0x56,
+        0x0e, 0x76, 0x4d, 0x88, 0x60, 0xb2, 0xe1, 0x7a, 0x3c, 0xae, 0x3a, 0xc6, 0x66, 0xbf, 0x99,
+        0x23, 0xe9,
+    ]));
 
     let progress = AtomicU64::new(0);
     let count = AtomicU64::new(0);
-    let real_count = AtomicU64::new(0);
 
     let t = Instant::now();
 
-    let mut result_counts = HashMap::<_, u64>::default();
     let mut results = HashMap::<_, Archive<_>>::default();
 
-    packings.iter().take(100).for_each(|&packing| {
+    packings.iter().take(10).for_each(|&packing| {
         let packing: Vec<pcf::Placement> = packing.into_iter().map(From::from).collect();
         let hurdles = packing.iter().fold(0, |a, p| a | p.kind.hurdles());
 
-        let mut t_candidates = packing.iter()
+        let mut t_candidates = packing
+            .iter()
             .filter(|p| t_placement_may_tspin(p, hurdles, &packing));
         let only_t = t_candidates.next().xor(t_candidates.next()).copied();
 
-        let mut i_candidates = packing.iter()
+        let mut i_candidates = packing
+            .iter()
             .filter(|p| i_placement_may_tetris_pc(p, hurdles));
         let only_i = i_candidates.next().xor(i_candidates.next()).copied();
-
-        let mut non_dominated = HashMap::<_, Archive<_>>::default();
 
         find_placement_sequences(
             &mut vec![],
@@ -50,17 +54,16 @@ pub fn compute_placements(packings_file: PathBuf) {
             &mut |placement, score, time, b2b| {
                 let order: [_; 15] = std::array::from_fn(|i| (placement[i].piece as usize).into());
                 let order = PackedPieceSeq::from(order);
-                *result_counts.entry(order).or_default() += 1;
-                non_dominated.entry(order).or_default().add(Entry::new(
-                    score as u16,
-                    time as u16,
-                    b2b,
-                ));
+
+                let packed_placements = <[_; 15]>::try_from(placement).unwrap().map(|p| p.pack());
+
                 results.entry(order).or_default().add(Entry::new(
                     score as u16,
                     time as u16,
                     b2b,
+                    packed_placements,
                 ));
+
                 count.fetch_add(1, Ordering::Relaxed);
             },
             0,
@@ -72,11 +75,6 @@ pub fn compute_placements(packings_file: PathBuf) {
             only_i,
         );
 
-        let nondom = non_dominated
-            .values()
-            .map(|archive| archive.len() as u64)
-            .sum();
-        let real_count = real_count.fetch_add(nondom, Ordering::Relaxed) + nondom;
         let prog = progress.fetch_add(1, Ordering::Relaxed) + 1;
 
         // if prog % 1_000 == 0 {
@@ -84,9 +82,8 @@ pub fn compute_placements(packings_file: PathBuf) {
         let d = t.elapsed();
         let eta = d.as_secs_f64() / prog as f64 * (packings.len() - prog as usize) as f64;
         print!(
-            "    {count} ({real_count}) placement seqs found (x{:.2} (x{:.2}) amplification), eta {:3}:{:02}:{:02}\r",
+            "    {count} placement seqs found (x{:.2} amplification), eta {:3}:{:02}:{:02}\r",
             count as f64 / prog as f64,
-            real_count as f64 / prog as f64,
             (eta / 3600.0).round(),
             (eta / 60.0 % 60.0).round(),
             (eta % 60.0).round(),
@@ -96,9 +93,9 @@ pub fn compute_placements(packings_file: PathBuf) {
     });
 
     println!();
-    println!("{} sequences", result_counts.len());
+    println!("{} piece sequences", results.len());
     println!(
-        "{} non-dominated (x{:.2} amplification)",
+        "{} non-dominated solutions (x{:.2} amplification)",
         results.values().map(|archive| archive.len()).sum::<usize>(),
         results.values().map(|archive| archive.len()).sum::<usize>() as f64
             / progress.into_inner() as f64,
@@ -111,13 +108,15 @@ pub fn compute_placements(packings_file: PathBuf) {
 struct Entry {
     score: u16,
     time_and_flags: u16,
+    placements: [u8; 15],
 }
 
 impl Entry {
-    fn new(score: u16, time: u16, b2b: bool) -> Self {
+    fn new(score: u16, time: u16, b2b: bool, placements: [u8; 15]) -> Self {
         Entry {
             score,
             time_and_flags: time | (b2b as u16) << 15,
+            placements,
         }
     }
 
